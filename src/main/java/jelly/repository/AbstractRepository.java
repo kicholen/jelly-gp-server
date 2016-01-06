7eec42c58e450e323e55c8a6d167703d3d4d141e
Mateusz Debski <mateuszdeb@gmail.com>
2016-01-06 23:51:26 +0100

    close all entityManagers

--- src/main/java/jelly/repository/AbstractRepository.java
+++ src/main/java/jelly/repository/AbstractRepository.java
@@ -3,6 +3,7 @@ package jelly.repository;
 import jelly.config.EntityManagerCreator;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import java.io.Serializable;
 import java.util.List;
 
@@ -14,32 +15,82 @@ public abstract class AbstractRepository<T extends Serializable> {
     }
 
     public T findOne(final long id) {
-        return EntityManagerCreator.create().find(clazz, id);
+        EntityManager em = EntityManagerCreator.create();
+        try {
+            return em.find(clazz, id);
+        }
+        finally {
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
     }
 
     public List<T> findAll(String name) {
-        return EntityManagerCreator.create().createNamedQuery(name, clazz).getResultList();
+        EntityManager em = EntityManagerCreator.create();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            return em.createNamedQuery(name, clazz).getResultList();
+        }
+        finally {
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
     }
 
     public void create(final T entity) {
         EntityManager em = EntityManagerCreator.create();
-        em.getTransaction().begin();
-        em.persist(entity);
-        em.getTransaction().commit();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.persist(entity);
+            transaction.commit();
+        }
+        finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
     }
 
     public T update(final T entity) {
         EntityManager em = EntityManagerCreator.create();
-        em.getTransaction().begin();
-        em.merge(entity);
-        em.getTransaction().commit();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.merge(entity);
+            transaction.commit();
+        }
+        finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
         return entity;
     }
 
     public void delete(final long entityId) {
         EntityManager em = EntityManagerCreator.create();
-        em.getTransaction().begin();
-        em.remove(em.getReference(clazz, entityId));
-        em.getTransaction().commit();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.remove(em.getReference(clazz, entityId));
+            transaction.commit();
+        }
+        finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
     }
 }
